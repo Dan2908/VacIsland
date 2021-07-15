136bc03f4e8c2f5665a9b8f6ca56a74e47cf1cc2
Dan2908 <danilobrandolin29@gmail.com>
2021-07-15 17:21:16 -0300

    Created window

--- vacation.cpp
+++ vacation.cpp
@@ -1,14 +1,64 @@
 #include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#define LOG(...) std::cerr << __VA_ARGS__ << std::endl
 
+const int WIDTH = 800, HEIGHT = 600;
+
+/* Función para informar a OpenGL los cambios de dimensión y ubicación de la ventana.
+    Llama a glViewport() */
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+/* Entrada de teclado */
+void processInput(GLFWwindow *window);
 
 int main(){
+//Iniciar GLFW
     if(!glfwInit()){
-        perror("Error al iniciar glfw");
-        return -1;
+        LOG("Error al iniciar glfw");
+        return EXIT_FAILURE;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+// Creación de ventana
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Vacation Island", NULL, NULL);
+    if(window == NULL){
+        LOG("Error creando ventana");
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent(window); // poner en contexto principal
+
+    if(glewInit() != GLEW_OK){
+        LOG("Error al iniciar GLEW");
+        return EXIT_FAILURE;
     }
 
-    perror("FIN");
+    glViewport(0,0,WIDTH, HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while(!glfwWindowShouldClose(window)){
+    // input
+        processInput(window);
+    // rendering
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    // events and swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return EXIT_SUCCESS;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, true);
+    }
 }
