1e59c0521be18bed5161c34d5cefa4dd75bd37d1
Dan2908 <danilobrandolin29@gmail.com>
2021-07-24 14:54:10 -0300

    Update sbox.cpp

--- sbox.cpp
+++ sbox.cpp
@@ -3,78 +3,70 @@
 
 #define LOG(...) std::cerr << __VA_ARGS__ << std::endl
 
-template<typename T>
-struct m_pos{
-    T x, y, z;
-    m_pos(T x, T y, T z) : x(x), y(y), z(z) {}
-    m_pos(T x, T y) : x(x), y(y), z(0) {}
+class m_pos{
+public:
+    float x, y, z;
+    m_pos(float x, float y, float z) : x(x), y(y), z(z) {}
+    m_pos(float x, float y) : x(x), y(y), z(0) {}
     ~m_pos() {}
-    m_pos<T> operator+(m_pos<T> A){
-        return m_pos<T>(x + A.x, y + A.y, z + A.z);
+    friend m_pos operator+(m_pos one, m_pos other){
+        return m_pos(one.x + other.x, one.y + other.y, one.z + other.z);
     }
 };
 
-
-
-template<typename T>
 class Point{
-    T *arr_PCT; // positions(3) colors(3) texture(2)
+    float *arr_PCT; // positions(3) colors(3) texture(2)
 public:
-    Point()             : arr_PCT((T*)calloc(8, sizeof(T))) 
+    Point()             : arr_PCT((float*)calloc(8, sizeof(float))) 
     {}
-    Point(m_pos<T> pos) : Point()
+    Point(m_pos pos) : Point()
     { 
         set_pos(pos); 
     }
+    Point(const Point &p) : Point() { memcpy(arr_PCT, p.arr_PCT, sizeof(float) * 8); }
     ~Point() 
     { 
         free(arr_PCT);
     }
     // methods //
-    T* getArray()
+    float* getArray()
     {
         return arr_PCT;
     }
-    void set_pos(m_pos<T> pos){
+    void set_pos(m_pos pos){
         arr_PCT[0] = pos.x;
         arr_PCT[1] = pos.y;
         arr_PCT[2] = pos.z;
     }
-    void set_color(m_pos<T> color){
+    void set_color(m_pos color){
         arr_PCT[3] = color.x;
         arr_PCT[4] = color.y;
         arr_PCT[5] = color.z;
     }
-    void set_texture(m_pos<T> texture){
+    void set_texture(m_pos texture){
         arr_PCT[6] = texture.x;
         arr_PCT[7] = texture.y;
     }
-    m_pos<T> get_pos(){
-        return m_pos<T>(arr_PCT[0], arr_PCT[1], arr_PCT[2]);
+    m_pos get_pos(){
+        return m_pos(arr_PCT[0], arr_PCT[1], arr_PCT[2]);
     }
-    Point* copy(m_pos<T> offset){
-        Point *ret = new Point();
-        ret->arr_PCT = arr_PCT;
-        LOG(offset.y << " <--");
-        ret->set_pos(offset + get_pos());
-        return ret;
+    void translate(m_pos pos){
+        set_pos(pos + get_pos());
     }
 };
 
-
 const char *path = "res/asset/cube.dat";
 
-void get_point(){
-
-}
-
 int main(){
-    Point<float> p( m_pos<float>(1, -7, -7) );
+    Point a( m_pos(-0.5f, -0.5f) );
+    Point b = a , c = a;
+    b.translate( m_pos(0.0f, 1.0f) );
+    c.translate( m_pos(1.0f, 0.0f) );
 
-    Point<float> *r = p.copy( m_pos<float>(0.0f, 1.5f, 0.0f) );
+    LOG(&a << " : " << &b);
+    LOG("{" << a.get_pos().x << ", " << a.get_pos().y << ", " << a.get_pos().z  << "}");
+    LOG("{" << b.get_pos().x << ", " << b.get_pos().y << ", " << b.get_pos().z  << "}");
+    LOG("{" << c.get_pos().x << ", " << c.get_pos().y << ", " << c.get_pos().z  << "}");
 
-    for(int i = 0; i < 8; i++){
-        LOG(r->getArray()[i]);
-    }
     return EXIT_SUCCESS;
 }
